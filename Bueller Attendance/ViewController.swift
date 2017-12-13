23d9b287ef2a722d0872e2fe5e97458cedbafe29
CLAIRE MYERS <764779@ehs-s-232-36.local>
2017-12-13 12:32:24 -0600

    Picker View finished CM

--- Bueller Attendance/ViewController.swift
+++ Bueller Attendance/ViewController.swift
@@ -8,14 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var PickerLabel: UILabel!
+    @IBOutlet weak var SchedulePicker: UIPickerView!
+    
+    let schedules = ["Normal", "Flex", "Pepfest", "Finals Day 1", "Finals Day 2"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return schedules[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return schedules.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        PickerLabel.text = schedules[row]
+    }
+    
+    @IBOutlet weak var DateText: UILabel!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let date = Date()
+        let calendar = Calendar.current
+        
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        let second = calendar.component(.second, from: date)
+        print("\(hour):\(minute):\(second)")
+        
+        let day = calendar.component(.day, from: date)
+        let month = calendar.component(.month, from: date)
+        print("\(day).\(month)")
+        
+        DateText.text =  "\(month)/\(day)"
+        
+        
+        
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
